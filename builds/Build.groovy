3a0fb0a87fc8aa10794a827075004c3cb300baa1
B45617 <machavezdominguez1@gmail.com>
2025-10-23 09:10:39 -0500

    changes 23/20/2025

--- builds/Build.groovy
+++ builds/Build.groovy
@@ -26,27 +26,55 @@ println("=== Compilando ${member} ===")
 // Copiar fuente al PDS
 new CopyToPDS().file(srcFile).dataset(props.srcPDS).member(member).execute()
 
+def sysoutPath = "${props.workspace}/builds/sysout_${member}.txt"
+def sysoutFile = new File(sysoutPath)
+if (!sysoutFile.exists()) {
+    sysoutFile.createNewFile()
+}
+
+def sysUt1Path = "${props.workspace}/builds/sysut1_${member}.tmp"
+def sysut1File = new File(sysUt1Path)
+if (!sysut1File.exists()) {
+    sysut1File.createNewFile()
+}
+
 // === Compilar COBOL ===
-def compile = new MVSExec().pgm(props.cobol.compiler)
+def compile = new MVSExec().pgm(props.cobolCompiler)
 compile.dd(new DDStatement().name("SYSIN").dsn("${props.srcPDS}(${member})").options("shr"))
-compile.dd(new DDStatement().name("SYSLIN").dsn("${props.objPDS}(${member})").options("shr"))
-compile.dd(new DDStatement().name("SYSLIB").dsn(props.copybookDir).options("shr"))
-compile.dd(new DDStatement().name("SYSOUT").options("sysout=*"))
-compile.dd(new DDStatement().name("SYSUT1").options("unit(vio) space(4096,(50,50))"))
-compile.parm(props.cobol.compileParms)
+//compile.dd(new DDStatement().name("SYSLIN").dsn("${props.objPDS}(${member})").options("shr"))
+compile.dd(new DDStatement().name("SYSLIN").dsn("${props.objPDS}(${member})").options("new"))
+//compile.dd(new DDStatement().name("SYSLIB").dsn(props.copybookDir).options("shr"))
+compile.dd(new DDStatement().name("SYSLIB").path(props.copybookDir))
+//compile.dd(new DDStatement().name("SYSOUT").options("sysout=*"))
+//compile.dd(new DDStatement().name("SYSOUT").path(sysoutPath))
+compile.dd(new DDStatement().name("SYSOUT").path(sysoutPath).output(true))
+//compile.dd(new DDStatement().name("SYSUT1").path(sysUt1Path))
+// compile.dd(new DDStatement().name("SYSUT1").options("unit(sysda) space(4096,(50,50))"))
+// compile.dd(new DDStatement().name("SYSUT2").options("unit(sysda) space(4096,(50,50))"))
+// compile.dd(new DDStatement().name("SYSUT3").options("unit(sysda) space(4096,(50,50))"))
+// compile.dd(new DDStatement().name("SYSUT4").options("unit(sysda) space(4096,(50,50))"))
+
+compile.parm(props.cobolCompileParms)
 
 def rc = compile.execute()
 if (rc > 4) {
-    println("Error de compilación RC=${rc}")
+    println("Error de compilacion RC=${rc}")
     System.exit(rc)
 }
 
+def linkPrintPath  = "${props.workspace}/builds/link_${member}.txt"
+def linkPrintFile = new File(linkPrintPath)
+if (!linkPrintFile.exists()) {
+    linkPrintFile.createNewFile()
+}
 // === Link-edit ===
-def link = new MVSExec().pgm(props.cobol.linkEditor)
+def link = new MVSExec().pgm(props.cobolLinkEditor)
 link.dd(new DDStatement().name("SYSLMOD").dsn("${props.loadPDS}(${member})").options("shr"))
 link.dd(new DDStatement().name("SYSLIN").dsn("${props.objPDS}(${member})").options("shr"))
-link.dd(new DDStatement().name("SYSPRINT").options("sysout=*"))
-link.parm(props.cobol.linkEditParms)
+//link.dd(new DDStatement().name("SYSPRINT").options("sysout=*"))
+//link.dd(new DDStatement().name("SYSPRINT").path(linkPrintPath))
+link.dd(new DDStatement().name("SYSPRINT").path(linkPrintPath).output(true))
+link.parm(props.cobolLinkEditParms)
 
 def rcLink = link.execute()
 if (rcLink > 4) {
