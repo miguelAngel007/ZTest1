3ebcc3013e64e93051e9e4ef0a00cd082f4f92bc
Chavez Dominguez <machavezdominguez1@gmail.com>
2025-11-07 18:02:57 -0500

    end

--- builds/Build.groovy
+++ builds/Build.groovy
@@ -1,7 +1,3 @@
-/*
- * Build.groovy - Script principal para compilar programas COBOL con DBB
- */
-
 import com.ibm.dbb.build.*
 
 def props = BuildProperties.getInstance()
@@ -18,68 +14,55 @@ def srcFile = new File("${props.sourceDir}/${program}")
 
 println("=== Compilando ${member} ===")
 
-// Crear datasets si no existen
-// new CreatePDS().dataset(props.srcPDS).options("DSNTYPE(PDS) RECFM(FB) LRECL(80) BLKSIZE(3120)").create()
-// new CreatePDS().dataset(props.objPDS).options("DSNTYPE(PDS) RECFM(FB) LRECL(80) BLKSIZE(3120)").create()
-// new CreatePDS().dataset(props.loadPDS).options("DSNTYPE(PDS) DSORG(PO) RECFM(U) BLKSIZE(32760)").create()
-
 // Copiar fuente al PDS
 new CopyToPDS().file(srcFile).dataset(props.srcPDS).member(member).execute()
 
-def sysoutPath = "${props.workspace}/builds/sysout_${member}.txt"
-def sysoutFile = new File(sysoutPath)
-if (!sysoutFile.exists()) {
-    sysoutFile.createNewFile()
-}
+// Rutas de salida de log
+def sysoutLog = new File("${props.workspace}/builds/${member}_cbl.log")
+if (!sysoutLog.exists()) sysoutLog.createNewFile()
 
-def sysUt1Path = "${props.workspace}/builds/sysut1_${member}.tmp"
-def sysut1File = new File(sysUt1Path)
-if (!sysut1File.exists()) {
-    sysut1File.createNewFile()
-}
+// Compilación COBOL
+def compile = new MVSExec().pgm(props.cobolCompiler).parm("LIB")
 
-// === Compilar COBOL ===
-def compile = new MVSExec().pgm(props.cobolCompiler)
 compile.dd(new DDStatement().name("SYSIN").dsn("${props.srcPDS}(${member})").options("shr"))
-//compile.dd(new DDStatement().name("SYSLIN").dsn("${props.objPDS}(${member})").options("shr"))
 compile.dd(new DDStatement().name("SYSLIN").dsn("${props.objPDS}(${member})").options("new"))
-//compile.dd(new DDStatement().name("SYSLIB").dsn(props.copybookDir).options("shr"))
-compile.dd(new DDStatement().name("SYSLIB").path(props.copybookDir))
-//compile.dd(new DDStatement().name("SYSOUT").options("sysout=*"))
-//compile.dd(new DDStatement().name("SYSOUT").path(sysoutPath))
-compile.dd(new DDStatement().name("SYSOUT").path(sysoutPath).output(true))
-//compile.dd(new DDStatement().name("SYSUT1").path(sysUt1Path))
-// compile.dd(new DDStatement().name("SYSUT1").options("unit(sysda) space(4096,(50,50))"))
-// compile.dd(new DDStatement().name("SYSUT2").options("unit(sysda) space(4096,(50,50))"))
-// compile.dd(new DDStatement().name("SYSUT3").options("unit(sysda) space(4096,(50,50))"))
-// compile.dd(new DDStatement().name("SYSUT4").options("unit(sysda) space(4096,(50,50))"))
 
-compile.parm(props.cobolCompileParms)
+// SYSUT1..SYSUT17
+(1..17).each { num ->
+    compile.dd(new DDStatement().name("SYSUT$num").options("cyl space(5,5) unit(vio) new"))
+}
+
+// SYSMDECK y SYSPRINT
+compile.dd(new DDStatement().name("SYSMDECK").options("cyl space(5,5) unit(vio) new"))
+compile.dd(new DDStatement().name("SYSPRINT").options("cyl space(5,5) unit(vio) new"))
+compile.copy(new CopyToHFS().ddName("SYSPRINT").file(sysoutLog))
+
+// Librería del compilador
+compile.dd(new DDStatement().name("TASKLIB").dsn(props.cobolCompilerDS).options("shr"))
 
 def rc = compile.execute()
 if (rc > 4) {
-    println("Error de compilacion RC=${rc}")
+    println("Error de compilacion RC=$rc. Revisa el log: ${sysoutLog}")
     System.exit(rc)
+} else {
+    println("Compilación exitosa RC=$rc. Log: ${sysoutLog}")
 }
 
-def linkPrintPath  = "${props.workspace}/builds/link_${member}.txt"
-def linkPrintFile = new File(linkPrintPath)
-if (!linkPrintFile.exists()) {
-    linkPrintFile.createNewFile()
-}
-// === Link-edit ===
+// Link-edit
+def linkLog = new File("${props.workspace}/builds/${member}_link.log")
+if (!linkLog.exists()) linkLog.createNewFile()
+
 def link = new MVSExec().pgm(props.cobolLinkEditor)
 link.dd(new DDStatement().name("SYSLMOD").dsn("${props.loadPDS}(${member})").options("shr"))
 link.dd(new DDStatement().name("SYSLIN").dsn("${props.objPDS}(${member})").options("shr"))
-//link.dd(new DDStatement().name("SYSPRINT").options("sysout=*"))
-//link.dd(new DDStatement().name("SYSPRINT").path(linkPrintPath))
-link.dd(new DDStatement().name("SYSPRINT").path(linkPrintPath).output(true))
+link.dd(new DDStatement().name("SYSPRINT").options("cyl space(5,5) unit(vio) new"))
+link.copy(new CopyToHFS().ddName("SYSPRINT").file(linkLog))
 link.parm(props.cobolLinkEditParms)
 
 def rcLink = link.execute()
 if (rcLink > 4) {
-    println("Error en linkedit RC=${rcLink}")
+    println("Error en linkedit RC=$rcLink. Revisa el log: ${linkLog}")
     System.exit(rcLink)
+} else {
+    println("Link-edit completado RC=$rcLink. Log: ${linkLog}")
 }
-
-println("Compilación y linkedit de ${member} completados correctamente.")
